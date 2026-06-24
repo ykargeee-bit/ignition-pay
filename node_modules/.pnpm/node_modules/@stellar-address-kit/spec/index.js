c798e53871bba16c24d022e8f9550cf511acf96b
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-06-24 14:46:32 +0000

    Removed Node Modules

--- node_modules/.pnpm/node_modules/@stellar-address-kit/spec/index.js
+++
@@ -1,7 +0,0 @@
-const vectors = require('./vectors.json');
-const schema = require('./schema.json');
-
-module.exports = {
-  vectors,
-  schema
-};
--- node_modules/.pnpm/node_modules/stellar-address-kit/src/muxed/types.ts
+++
@@ -1,4 +0,0 @@
-export type MuxedResult = {
-  baseG: string;
-  id: bigint;
-};
